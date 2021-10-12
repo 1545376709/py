bf8745e3a2befbc9b054d6015456690e851770a5
wm <@>
2021-10-12 17:08:59 +0800

    upload part

--- upload.py
+++ upload.py
@@ -4,13 +4,15 @@ import requests
 
 
 def main():
-    upload_url = ''
+    upload_url = 'localhost:8081/dev/upload'
     for f in os.listdir('f'):
         if f.find('.json') > 0:
             jso = json.load(open('f/{}'.format(f), 'r', encoding='utf-8'))
-            # print(jso)
-            import subprocess
-            subprocess.call('curl' )
+            print(jso)
+            files = {'file': open('f/{}.mp4'.format(jso["bvid"]), 'rb'),
+                     'pic': open('f/{}.jpg'.format(jso["bvid"]), 'rb')
+                     }
+            resp = requests.post(upload_url, files, jso)
 
 
 if __name__ == '__main__':
